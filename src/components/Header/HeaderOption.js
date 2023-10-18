763f02042fd6b7c83244c8c7487a7e114df27692
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-10-18 14:13:32 +0530

    project intailization

---
+++ src/components/Header/HeaderOption.js
@@ -0,0 +1,12 @@
+import './HeaderOption.css'
+ 
+function HeaderOption({ avtar , Icon , title}) {
+  return (
+    <div className="headerOption">
+      {Icon && <Icon className="headerOption_icon" />}
+      <h3 className='headerOption_title'>{title}</h3>
+    </div>
+  ); 
+}
+
+export default HeaderOption
