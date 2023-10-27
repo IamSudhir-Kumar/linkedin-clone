25b5bb399b366d62f9bd043f60aea283fcc2171e
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-10-27 21:26:39 +0530

    modal input configure

--- src/Components/Common/Modal.jsx
+++ src/Components/Common/Modal.jsx
@@ -1,50 +1,20 @@
 
-const Modal = ({ modalOpen, setModalOpen}) => {
+import { Modal } from 'antd';
+const ModalComponent = ({modalOpen, setModalOpen}) => {
   return (
-    <div className="p-4 space-y-4">
-
-      {modalOpen && (
-        <div className="fixed top-20 left-1/2 transform -translate-x-1/2">
-          <div className="bg-white p-4 rounded shadow-md">
-            <h2 className="text-xl font-semibold mb-4">20px to Top</h2>
-            <p>some contents...</p>
-            <p>some contents...</p>
-            <p>some contents...</p>
-            <button
-              onClick={() => setModalOpen(false)}
-              className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mt-4"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      )}
-
-      <button
-        onClick={() => setModalOpen(true)}
-        className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+    <>
+      <Modal
+        title="Vertically centered modal dialog"
+        centered
+        open={modalOpen}
+        onOk={() => setModalOpen(false)}
+        onCancel={() => setModalOpen(false)}
       >
-        Vertically centered modal dialog
-      </button>
-
-      {modalOpen && (
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <div className="bg-white p-4 rounded shadow-md">
-            <h2 className="text-xl font-semibold mb-4">Vertically centered modal dialog</h2>
-            <p>some contents...</p>
-            <p>some contents...</p>
-            <p>some contents...</p>
-            <button
-              onClick={() => setModalOpen(false)}
-              className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mt-4"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
+        <p>some contents...</p>
+        <p>some contents...</p>
+        <p>some contents...</p>
+      </Modal>
+    </>
   );
 };
-
-export default Modal;
+export default ModalComponent;
--- src/Components/Common/PostStatus.jsx
+++ src/Components/Common/PostStatus.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import Modal from './Modal';
+import ModalComponent from './Modal';
 import { useState } from 'react';
 const PostStatus = () => {
-    const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
   return (
-   <>
-   <div className="flex items-center justify-center">
-  <div className="bg-white mt-3  shadow-md rounded-md w-full h-32 max-w-xl max-h-64 border-2 border-gray-200 flex justify-center items-center">
-  <button className=' bg-white hover:bg-gray-100 border border-gray-300 text-gray-400 w-10/12 rounded-full h-14 text-left pl-3 overflow-hidden mb-10  font-semibold font-Assistant' onClick={() => setModalOpen}>Start a post</button>
-  </div>
-</div>
-<Modal modalOpen={modalOpen} setModalOpen={setModalOpen}/>
-   </>
+    <div>
+      <div className="flex items-center justify-center">
+        <div className="bg-white mt-3  shadow-md rounded-md w-full h-32 max-w-xl max-h-64 border-2 border-gray-200 flex justify-center items-center">
+          <button className=' bg-white hover:bg-gray-100 border
+           border-gray-300 text-gray-400 w-10/12 rounded-full h-14 
+           text-left pl-3 overflow-hidden mb-10  font-semibold font-Assistant'
+           onClick={() => setModalOpen(true)}>Start a post</button>
+        </div>
+      </div>
+      <ModalComponent modalOpen={modalOpen} setModalOpen={setModalOpen} />
+    </div>
   );
 };
 
