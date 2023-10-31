edf7686f96015e59b6baaa166eb18e0c2e075500
SUDHIR KUMAR <112376748+IamSudhir-Kumar@users.noreply.github.com>
2023-10-31 14:14:40 +0530

    continued in different device

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import HomePageComponent from '../components/HomePageComponent'
+import { getUserName } from '../api/FireStore'
 import Navbar from '../Components/Common/Navbar'
 export default function HomeLayout() {
+  useMemo(() => {
+    getUserName;
+  }, []);
   return (
     <div className="bg-gray-300">
     <Navbar />
